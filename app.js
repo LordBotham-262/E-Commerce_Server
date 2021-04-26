22f7ea792b86f0d7b7f11d3b4d74c0c558808bc6
Dhaval <dhavu262@gmail.com>
2021-04-26 11:53:31 +0530

    cartItems api

--- app.js
+++ app.js
@@ -19,43 +19,40 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
-connection.query('select * from posts', function (error, results, fields) {
-  if (error) throw error;
-  posts = results;
-});
 
-app.route('/products_type')
+app.route('/product_type')
   .get(function(req,res){
     connection.query('select * from products_type' ,function (error, results, fields) {
        if (error) throw error;
        else {
+         console.log("Query made for product types" );
          res.send(results);
        }
      });
   })
   ;
 
-  app.route('/product')
+app.route('/product/category_id/:categoryId/product_id/:productId')
   .get(function(req,res){
-    connection.query('select * from product' ,function (error, results, fields) {
+    connection.query('select * from product where (id = ? or ? = 0) and (category = ? or ?  = 0 )',[req.params.productId,req.params.productId,req.params.categoryId,req.params.categoryId],function (error, results, fields) {
        if (error) throw error;
        else {
-         res.send(results);
-       }
-     });
-  })
-;
-
-
-app.route('/product/:id')
-  .get(function(req,res){
-    connection.query('select * from product where id = ?',[req.params.id],function (error, results, fields) {
-       if (error) throw error;
-       else {
+         console.log("Query made for category type " + req.params.categoryId + " & product_type " + req.params.productId );
          res.send(results);
        }
      });
   });
+
+  app.route('/cart/user_id/:userId')
+    .get(function(req,res){
+      connection.query('SELECT * FROM cart_items AS c INNER JOIN product AS p ON c.product_id = p.id WHERE user_id = ?',[req.params.userId],function (error, results, fields) {
+         if (error) throw error;
+         else {
+           console.log("Query made for cart of user " + req.params.userId);
+           res.send(results);
+         }
+       });
+    });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
