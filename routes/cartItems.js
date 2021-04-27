e811f1f87266dfa4174a378ea33fc0cf442c40a7
Dhaval <dhavu262@gmail.com>
2021-04-28 02:47:07 +0530

    still issue with the productValidate

--- routes/cartItems.js
+++ routes/cartItems.js
@@ -16,65 +16,13 @@ router.get('/:userId', (req, res, next) => {
 
 
 router.post('/:userId', userValidate, (req, res, next) => {
-let xyz;
   req.body.cartItems.forEach((item, i) => {
-    xyz = productValidate(item, req, res, next)
-    .then(console.log(xyz));
+    productValidate(item, req, res, next)
+      .then(function(data) {
+        console.log(data);
+        //next();
+      });
   });
-
-  // if (xyz) {
-  //   res.status(201).json({
-  //     message: 'Items added to cart for User ' + req.params.userId
-  //   });
-  // }
-
-  // res.status(201).json({
-  //   message: 'Items added to cart for User ' + req.params.userId
-  // });
-  // try {
-  //   connection.query('SELECT * from user where id = ?', [req.params.userId], function(error, user, fields) {
-  //     if (error) {
-  //       throw error;
-  //     } else {
-  //       if (user.length == 0) {
-  //         res.status(404).json({
-  //           message: 'user not found for ID ' + req.params.userId
-  //         });
-  //       } else {
-  //         req.body.cartItems.forEach((item, i) => {
-  //           connection.query('SELECT * from product where id = ?', [item.product_id], function(error, product, fields) {
-  //             if (error) {
-  //               res.status(404).json({
-  //                 message: 'SQL Error ' + error.sqlMessage
-  //               });
-  //             } else {
-  //               if (product.length == 0) {
-  //                 res.status(404).json({
-  //                   message: 'Product not found for ID ' + item.product_id
-  //                 });
-  //               } else {
-  //                 connection.query('INSERT INTO cart_items (product_id,user_id,size,quantity) VALUES (?,?,?,?)', [item.product_id, req.params.userId, item.size, item.quantity], function(error, results, fields) {
-  //                   if (error)
-  //                     throw error;
-  //                   else {
-  //                     console.log("Items inserted successfully");
-  //                   }
-  //                 })
-  //               }
-  //             }
-  //           })
-  //         })
-  //         res.status(201).json({
-  //           message: 'Items added to cart for User ' + req.params.userId
-  //         });
-  //       }
-  //     }
-  //   })
-  // } catch (e) {
-  //   console.log(e);
-  //   next();
-  // }
-
 });
 
 
@@ -95,35 +43,114 @@ function userValidate(req, res, next) {
   })
 }
 
-async function productValidate(item, req, res, next) {
-  let productValidation;
-  connection.query('SELECT * from product where id = ?', [item.product_id], function(error, product, fields) {
-    if (error) {
-      res.status(500).json({
-        message: error.sqlMessage
-      });
-    } else {
-      if (product.length == 0) {
-        res.status(404).json({
-          message: 'Product not found with ID ' + item.product_id
+function productValidate(item, req, res, next) {
+  return new Promise(function(resolve, reject) {
+    let productValidation;
+    connection.query('SELECT * from product where id = ?', [item.product_id], function(error, product, fields) {
+      if (error) {
+        res.status(500).json({
+          message: error.sqlMessage
         });
-        productValidation = false;
       } else {
-        connection.query('INSERT INTO cart_items (product_id,user_id,size,quantity) VALUES (?,?,?,?)', [item.product_id, req.params.userId, item.size, item.quantity], function(error, results, fields) {
-          if (error) {
-            res.status(500).json({
-              message: error.sqlMessage
-            });
-          } else {
-            console.log("Items inserted successfully");
-            productValidation =  true;
-          }
-        })
-      };
-    }
-  })
-  return productValidation;
+        if (product.length == 0) {
+          res.status(404).json({
+            message: 'Product not found with ID ' + item.product_id
+          });
+          productValidation = false;
+        } else {
+          connection.query('INSERT INTO cart_items (product_id,user_id,size,quantity) VALUES (?,?,?,?)', [item.product_id, req.params.userId, item.size, item.quantity], function(error, results, fields) {
+            if (error) {
+              res.status(500).json({
+                message: error.sqlMessage
+              });
+            } else {
+              console.log("Items inserted successfully");
+              productValidation = true;
+            }
+          })
+        };
+      }
+    })
+    resolve(productValidation) ;
+  });
 }
 
 
 module.exports = router;
+
+
+
+//   function username() {
+//       console.log("agyaaa");
+//       return new Promise(function(resolve,reject) {
+//           User.findOne({"username":req.body.username}, function(err,user) {
+//               if (err) {
+//                   reject(err)
+//               } else {
+//                   console.log("yaha b agyaaa");
+//                   var errorsArr = [];
+//                   errorsArr.push({"msg":"Username already been taken."});
+//                   resolve(errorsArr);
+//               }
+//
+//           });
+//       });
+//   }
+// username().then(function(data) {
+//     console.log(data);
+//     next();
+// });
+// });
+
+// if (xyz) {
+//   res.status(201).json({
+//     message: 'Items added to cart for User ' + req.params.userId
+//   });
+// }
+
+// res.status(201).json({
+//   message: 'Items added to cart for User ' + req.params.userId
+// });
+// try {
+//   connection.query('SELECT * from user where id = ?', [req.params.userId], function(error, user, fields) {
+//     if (error) {
+//       throw error;
+//     } else {
+//       if (user.length == 0) {
+//         res.status(404).json({
+//           message: 'user not found for ID ' + req.params.userId
+//         });
+//       } else {
+//         req.body.cartItems.forEach((item, i) => {
+//           connection.query('SELECT * from product where id = ?', [item.product_id], function(error, product, fields) {
+//             if (error) {
+//               res.status(404).json({
+//                 message: 'SQL Error ' + error.sqlMessage
+//               });
+//             } else {
+//               if (product.length == 0) {
+//                 res.status(404).json({
+//                   message: 'Product not found for ID ' + item.product_id
+//                 });
+//               } else {
+//                 connection.query('INSERT INTO cart_items (product_id,user_id,size,quantity) VALUES (?,?,?,?)', [item.product_id, req.params.userId, item.size, item.quantity], function(error, results, fields) {
+//                   if (error)
+//                     throw error;
+//                   else {
+//                     console.log("Items inserted successfully");
+//                   }
+//                 })
+//               }
+//             }
+//           })
+//         })
+//         res.status(201).json({
+//           message: 'Items added to cart for User ' + req.params.userId
+//         });
+//       }
+//     }
+//   })
+// } catch (e) {
+//   console.log(e);
+//   next();
+// }
