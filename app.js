676b163b1a26692184d02421a6ef411c6f0870a0
Dhaval <dhavu262@gmail.com>
2021-04-28 14:13:59 +0530

    adding heroku support file

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use((error,req,res,next) =>{
   });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
 
