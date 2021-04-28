2b0f05ee8a192c1343d916eb76c3cab93e460bac
Dhaval <dhavu262@gmail.com>
2021-04-28 13:49:08 +0530

    all APIs validation successfull

--- routes/cartItems.js
+++ routes/cartItems.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 var connection = require('./../database/serverConnector');
-var Q = require("q");
 
 router.get('/:userId', (req, res, next) => {
   connection.query('SELECT * FROM cart_items AS c INNER JOIN product AS p ON c.product_id = p.id WHERE user_id = ?', [req.params.userId], function(error, results, fields) {
@@ -16,10 +15,17 @@ router.get('/:userId', (req, res, next) => {
 
 
 router.post('/:userId', userValidate, (req, res, next) => {
-  multipleProductValidation(req, res, next).then(function(data) {
-    res.end();
-    console.log('stopping the res')
-  });
+ multipleProductValidation(req, res, next)
+  .then(function(data) {
+    res.status(201).json({
+      message: "Added all products to the cart"
+    });
+  })
+  .catch(error =>
+    res.status(500).json({
+      message: error
+    })
+  );
 });
 
 
@@ -41,40 +47,26 @@ function userValidate(req, res, next) {
 }
 
 function multipleProductValidation(req, res, next){
-  return new Promise(function(resolve, reject) {
-    req.body.cartItems.forEach((item, i) => {
-      productValidate(item, req, res, next)
-        .then(function(data) {
-          resolve(data);
-          console.log('validated object');
-          //next();
-        });
-    })
-  }
-  )}
+ return new Promise(function(resolve, reject) {
+   let promiseArr = req.body.cartItems.map(item => productValidate(item, req, res, next))
+   Promise.all(promiseArr).then(values => resolve(values)).catch(error =>reject(error))
+ }
+ )}
 
 
 function productValidate(item, req, res, next) {
   return new Promise(function(resolve, reject) {
     connection.query('SELECT * from product where id = ?', [item.product_id], function(error, product, fields) {
       if (error) {
-        res.status(500).json({
-          message: error.sqlMessage
-        });
+        reject(error.sqlMessage)
       } else {
         if (product.length == 0) {
-          res.status(404).json({
-            message: 'Product not found with ID ' + item.product_id
-          });
-          resolve(false);
+            reject('Product not found with ID ' + item.product_id)
         } else {
           connection.query('INSERT INTO cart_items (product_id,user_id,size,quantity) VALUES (?,?,?,?)', [item.product_id, req.params.userId, item.size, item.quantity], function(error, results, fields) {
             if (error) {
-              res.status(500).json({
-                message: error.sqlMessage
-              });
+              reject(error.sqlMessage)
             } else {
-              res.status(201).write('Items added to cart for User ' + req.params.userId);
               resolve(true)
             }
           })
@@ -85,80 +77,3 @@ function productValidate(item, req, res, next) {
 }
 
 module.exports = router;
-
-
-
-//   function username() {
-//       console.log("agyaaa");
-//       return new Promise(function(resolve,reject) {
-//           User.findOne({"username":req.body.username}, function(err,user) {
-//               if (err) {
-//                   reject(err)
-//               } else {
-//                   console.log("yaha b agyaaa");
-//                   var errorsArr = [];
-//                   errorsArr.push({"msg":"Username already been taken."});
-//                   resolve(errorsArr);
-//               }
-//
-//           });
-//       });
-//   }
-// username().then(function(data) {
-//     console.log(data);
-//     next();
-// });
-// });
-
-// if (xyz) {
-//   res.status(201).json({
-//     message: 'Items added to cart for User ' + req.params.userId
-//   });
-// }
-
-// res.status(201).json({
-//   message: 'Items added to cart for User ' + req.params.userId
-// });
-// try {
-//   connection.query('SELECT * from user where id = ?', [req.params.userId], function(error, user, fields) {
-//     if (error) {
-//       throw error;
-//     } else {
-//       if (user.length == 0) {
-//         res.status(404).json({
-//           message: 'user not found for ID ' + req.params.userId
-//         });
-//       } else {
-//         req.body.cartItems.forEach((item, i) => {
-//           connection.query('SELECT * from product where id = ?', [item.product_id], function(error, product, fields) {
-//             if (error) {
-//               res.status(404).json({
-//                 message: 'SQL Error ' + error.sqlMessage
-//               });
-//             } else {
-//               if (product.length == 0) {
-//                 res.status(404).json({
-//                   message: 'Product not found for ID ' + item.product_id
-//                 });
-//               } else {
-//                 connection.query('INSERT INTO cart_items (product_id,user_id,size,quantity) VALUES (?,?,?,?)', [item.product_id, req.params.userId, item.size, item.quantity], function(error, results, fields) {
-//                   if (error)
-//                     throw error;
-//                   else {
-//                     console.log("Items inserted successfully");
-//                   }
-//                 })
-//               }
-//             }
-//           })
-//         })
-//         res.status(201).json({
-//           message: 'Items added to cart for User ' + req.params.userId
-//         });
-//       }
-//     }
-//   })
-// } catch (e) {
-//   console.log(e);
-//   next();
-// }
