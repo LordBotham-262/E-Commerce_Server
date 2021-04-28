52d44d384da5a8e387e966e2b4dc1acf671d374a
Dhaval <dhavu262@gmail.com>
2021-04-28 17:37:00 +0530

    Database connection test

--- database/serverConnector.js
+++ database/serverConnector.js
@@ -1,9 +1,10 @@
 var mysql = require('mysql');
 var connection  = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'patel_'
+  host: 'remotemysql.com',
+  port: '3306',
+  user: process.env.user || 'root',
+  password: process.env.password || 'root',
+  database: process.env.databse || 'patel_'
 });
 
 //connection.connect();
