9a6ece1186b1a0a22e7a1ddce938e41ddaf4b965
Dhaval <dhavu262@gmail.com>
2021-04-29 00:38:59 +0530

    test 5

--- database/serverConnector.js
+++ database/serverConnector.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 
 var mysql = require('mysql');
-console.log(process.env.host);
-
-var connection  = mysql.createConnection({
+var pool  = mysql.createPool({
   host: process.env.host ,
   port: process.env.port,
   user: process.env.user,
@@ -11,19 +9,18 @@ var connection  = mysql.createConnection({
   database: process.env.databse
 });
 
-//connection.connect();
 
-databaseConnection().then(values => console.log(values)).catch(error => console.log(error));
+// databaseConnection().then(values => console.log(values)).catch(error => console.log(error));
+//
+// function databaseConnection() {
+//   return new Promise(function(resolve, reject) {
+//     connection.connect(function(err) {
+//       if (err) {
+//         reject(err)
+//       }
+//       resolve('Connected to the MySQL server.');
+//     });
+//   });
+// }
 
-function databaseConnection() {
-  return new Promise(function(resolve, reject) {
-    connection.connect(function(err) {
-      if (err) {
-        reject(err)
-      }
-      resolve('Connected to the MySQL server.');
-    });
-  });
-}
-
-module.exports = connection;
+module.exports = pool;
